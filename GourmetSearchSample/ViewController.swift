08a79d24c08b45c911ac91e59b0613ccbe0a794b
Yusuke Aono <yusukeaono.1031@gmail.com>
2015-08-06 20:00:19 +0900

    Create project

---
+++ GourmetSearchSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GourmetSearchSample
+//
+//  Created by Yusuke Aono on 8/6/15.
+//  Copyright (c) 2015 Yusuke Aono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
